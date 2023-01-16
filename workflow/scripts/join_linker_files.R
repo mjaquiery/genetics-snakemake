8852317b5f8d0c1f52abb39a9889ebb989180a11
Matt Jaquiery <matt.jaquiery@dtc.ox.ac.uk>
2023-01-16 10:03:55 +0000

    Rscript for combining linker files

---
+++ workflow/scripts/join_linker_files.R
@@ -0,0 +1,14 @@
+# Join together two linker files because of data corruption redownloading saga
+original <- commandArgs()[1]
+new <- commandArgs()[2]
+to <- commandArgs()[3]
+
+library('haven')
+library('tidyverse')
+
+o <- read.csv(original)
+n <- read_sav(new)
+
+out <- o %>% left_join(n, by="cidB2677", suffix=c('.x', ''))
+
+write.csv(out, to)
