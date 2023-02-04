22b4557b0940307b9239005618532f30ac81a2e3
Matt Jaquiery <matt.jaquiery@dtc.ox.ac.uk>
2023-02-04 04:49:24 +0000

    summary update

--- workflow/scripts/summarise_prs.R
+++ workflow/scripts/summarise_prs.R
@@ -20,46 +20,41 @@ d_all <- sources %>%
     scores = map(scores, ~ read_delim(paste0(root, .), delim = " ")),
     scores = map(scores, ~ select(., IID, Pt_1))  # just want PRS @ P-value=1
   ) %>%
-  mutate(
-    scores = map2(scores, source, function(scores, src) {
-      scores %>%
-        # deduplicate ids
-        mutate(IID = str_match(IID, "(.+)_\\1")[,2]) %>%
-        # split off role marker
-        separate(IID, c(src, "role"), -1)
-    })
-  )
+  unnest(scores) %>%
+  separate(IID, c("id", "role"), -1) %>%
+  rename(PRS = Pt_1)
 
 d_prs <- d_all %>%
+  nest(scores = c(id, PRS, role)) %>%
   mutate(
     scores = map2(source, scores, function(src, df) {
       link <- linkfile %>% select({src}, cidB2677) %>%
         mutate(across(everything(), as.character))
-      left_join(df, link, by = c(src)) %>%
-        filter(!is.na(cidB2677)) %>%
-        rename(IID = {src})
+      left_join(df, link, by = c("id" = src)) %>%
+        filter(!is.na(cidB2677))
     })
-  )
+  ) %>%
+  unnest(scores)
 
-prs_long <- bind_rows(d_prs$scores)
+prs_long <- d_prs %>% select(-prsice)
 prs_wide <- pivot_wider(
   prs_long,
   id_cols = cidB2677,
   names_from = role,
-  values_from = Pt_1
+  values_from = PRS
 )
 prs_wide_filtered <- prs_wide %>%
   filter(!is.na(M) & !is.na(`F`) & !is.na(A))
 prs_long_filtered <- prs_long %>%
   filter(cidB2677 %in% prs_wide_filtered$cidB2677) %>%
-  mutate(src = if_else(str_starts(IID, 'gi_'), 'g0p', 'g0m'))
+  mutate(src = if_else(str_starts(id, 'gi_'), 'g0p', 'g0m'))
 
 write_tsv(prs_wide_filtered, paste0(root, "prs.tsv"))
 
 # examine output
-range(prs_long$Pt_1)
+range(prs_long$PRS)
 
-ggplot(prs_long, aes(x = Pt_1, fill = role)) +
+ggplot(prs_long, aes(x = PRS, fill = role)) +
   geom_histogram(bins = 100, position = 'identity') +
   facet_wrap(~role) +
   theme_light()
